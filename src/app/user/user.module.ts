e5078971f7d29d5fb422d2b979529f46feb40265
saurabh <saurabhbharti9@gmail.com>
2020-06-13 21:40:37 +0530

    added signup design

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild([
       { path: 'signup', component: SignupComponent, pathMatch: 'full' },
     ]),
