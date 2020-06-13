9748d093bfd7201df4710694a7c4743c1700fc40
saurabh <saurabhbharti9@gmail.com>
2020-06-13 20:52:19 +0530

    added modules and routes

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { RouterModule, Routes } from '@angular/router';
+
+@NgModule({
+  declarations: [LoginComponent, SignupComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      { path: 'signup', component: SignupComponent, pathMatch: 'full' },
+    ]),
+  ],
+})
+export class UserModule {}
