5fd7c60fc986ba36c42444694b84cefeb6077774
saurabh <saurabhbharti9@gmail.com>
2020-06-21 17:25:58 +0530

    added userdetails ts

--- src/app/shared/user-details/user-details.component.ts
+++ src/app/shared/user-details/user-details.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  Input,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.css']
+  styleUrls: ['./user-details.component.css'],
 })
 export class UserDetailsComponent implements OnInit {
+  constructor() {}
+  @Input() userFirstName: any;
+  @Input() userLastName: String;
+  @Input() userStatus: String;
+  @Input() messageRead: String;
+  @Input() userBg: String;
+  @Input() userColor: String;
 
-  constructor() { }
+  public firstChar: String;
 
   ngOnInit(): void {
+    this.firstChar = this.userFirstName[0];
   }
-
 }
