af272d7037e5904bc3631ed00d12f46bf451a5f2
saurabh <saurabhbharti9@gmail.com>
2020-06-24 02:03:36 +0530

    added window-firstchar ts

--- src/app/shared/window-first-char/window-first-char.component.ts
+++ src/app/shared/window-first-char/window-first-char.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  SimpleChanges,
+  EventEmitter,
+  OnChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-window-first-char',
   templateUrl: './window-first-char.component.html',
-  styleUrls: ['./window-first-char.component.css']
+  styleUrls: ['./window-first-char.component.css'],
 })
 export class WindowFirstCharComponent implements OnInit {
+  //define the component inputs
+  @Input() name: String;
+  @Input() userColor: String;
+  @Input() userbg: String;
 
-  constructor() { }
+  //component will emit
+  @Output()
+  notify: EventEmitter<String> = new EventEmitter<String>();
+
+  public firstChar: String;
+  public _name: any;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this._name = this.name;
+    this.firstChar = this._name[0];
   }
 
+  //will modify the first char based on changes from parent component
+  ngOnChange(changes: SimpleChanges) {
+    let name = changes.name;
+    this._name = name.currentValue;
+    this.firstChar = this._name[0];
+  }
+  //will emit the name (reciever) to be toasted in parent
+  public currentUserClick(): any {
+    this.notify.emit(this._name);
+  }
 }
